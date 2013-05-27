14af71ad3804825abd2b9bf755887cc6102262e1
Ryan Schenk <rschenk@gmail.com>
2013-05-27 17:04:49 -0400

    Removed livereload

--- mockup_author.php
+++ mockup_author.php
@@ -245,8 +245,5 @@ if (isset($_GET['name']))
 
 	
 </script>
-
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
-
 </body>
 </html>
